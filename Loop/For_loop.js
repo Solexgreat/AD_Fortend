68bbf4d98d7238244f88265938d8ad140bf97b97
Solexgreat <solomonGodkid@gmail.com>
2023-02-23 13:15:53 +0100

    For loop

---
+++ Loop/For_loop.js
@@ -0,0 +1,13 @@
+for (i = 1; i <= 5; i++){
+    console.log(i);
+    if (i == 5){
+        console.log('Counting completed!')
+    }
+}
+
+for (i = 5; i >= 1; i -= 1 ){
+    console.log(i);
+    if (i == 1){
+        console.log('Countdown finished!')
+    }
+}
