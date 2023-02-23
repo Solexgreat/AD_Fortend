5ed4bab5ec44a4ff24595332a1c3485822e541b5
Solexgreat <solomonGodkid@gmail.com>
2023-02-23 13:30:38 +0100

    While Loop

---
+++ Loop/while_loop.js
@@ -0,0 +1,26 @@
+var i = 1;
+
+while (i <= 5){
+    console.log(i);
+    if (i == 5){
+        console.log('Counting Completed!');
+    }
+    i++;
+}
+
+var i = 5
+
+while (i >= 1){
+    console.log(i);
+    if (i == 1){
+        console.log('Countdown finished!');
+    }
+    i--;
+}
+
+var year = 2018;
+
+while (i <= 2022){
+    console.log(i);
+    year++;
+}
