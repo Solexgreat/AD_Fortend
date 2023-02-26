93ee4646354642ff1bd732f063dc15193e813f0c
Solexgreat <solomonGodkid@gmail.com>
2023-02-26 02:38:06 +0100

    Array

---
+++ JavaScript/array/exercise1.js
@@ -0,0 +1,19 @@
+var clothes = [];
+
+clothes.push('Dior');
+clothes.push('Gucci');
+clothes.push('Ankara');
+clothes.push('Jeans');
+clothes.push('GD');
+
+clothes.pop(clothes[4]);
+
+clothes.push('Shirt');
+
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = 'red';
+favCar.covertible = true;
+
+console.log(favCar);
