565dc00fbaf909b7de4dbb18edc5cf6d73402040
Solexgreat <solomonGodkid@gmail.com>
2023-05-29 04:06:21 +0100

    User event handling

--- React/customizing-example/src/App.js
+++ React/customizing-example/src/App.js
@@ -40,7 +40,7 @@ function App() {
   return (
     <div>
       <h1>Task: Add a button and handle a click event</h1>
-      <button> Guess the number between 1 and 3 </button>
+      <button onClick={handleClick} > Guess the number between 1 and 3 </button>
     </div>
   );
 }
