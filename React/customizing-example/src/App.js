5108825d88174b8dae916a1434b3a278931e0e74
Solexgreat <solomonGodkid@gmail.com>
2023-06-23 11:50:12 +0100

    Hook in react JS

--- React/customizing-example/src/App.js
+++ React/customizing-example/src/App.js
@@ -1,5 +1,6 @@
 import Heading from "./Heading";
 import Card from "./Card";
+
 /**import logo from './logo.svg';
 import './App.css';
 
@@ -51,5 +52,24 @@ function App() {
   );
 }
 
+
+
+export default function InputComponent() { 
+  const [inputText, setText] = useState('hello'); 
+
+  function handleChange(e) { 
+    setText(e.target.value); 
+  } 
+
+  return ( 
+    <> 
+      <input value={inputText} onChange={handleChange} /> 
+      <p>You typed: {inputText}</p> 
+      <button onClick={() => setText('hello')}> 
+        Reset 
+      </button> 
+    </> 
+  ); 
+} 
 
 export default App;
