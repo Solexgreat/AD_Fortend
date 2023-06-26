725ec237855ab8a161ae37878b550e32fd258576
Solexgreat <solomonGodkid@gmail.com>
2023-06-26 16:30:22 +0100

    Stateless and Statefull

--- React/customizing-example/src/state/StatelessApp.js
+++ React/customizing-example/src/state/StatelessApp.js
@@ -2,6 +2,12 @@ import Fruits from "./fruits";
 import FruitsCounter from "./FruitsCounter";
 
 function App() {
+  const [fruits] = React.useState([
+    {fruitName: 'apple', id: 1},
+    {fruitName: 'apple', id: 2},
+    {fruitName: 'plum', id: 3},
+]);
+
   return (
     <div className="App">
       <h1>Where should the state go?</h1>
