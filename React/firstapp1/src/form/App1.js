395413dbb9a34c976d6c82a1e19021027c3d175e
Solexgreat <solomonGodkid@gmail.com>
2023-07-26 15:55:48 +0100

    form pratice

--- React/firstapp1/src/form/App1.js
+++ React/firstapp1/src/form/App1.js
@@ -41,29 +41,41 @@ function App() {
                     <h2>Sign Up</h2>
                     <div className="Field">
                         <label>First Name <sup>*</sup> </label>
-                        <input value={firstName} placeholder="firstname"/>
+                        <input value={firstName}
+                        onChange={e => setFirstName(e.target.value)} 
+                        placeholder="firstname"/>
                     </div>
                     <div className="Field">
                         <label>Last Name <sup>*</sup> </label>
-                        <input placeholder="lastname"/>
+                        <input value={lastname}
+                        onChange=
+                        {e => setLastName(e.target.value)}
+                        placeholder="lastname"/>
                     </div>
                     <div className="Field">
                         <label>
                             Email <sup>*</sup>
                         </label>
-                        <input placeholder="email"/>
+                        <input value={email} 
+                        onChange={e => setEmail(e.target.value)} 
+                        placeholder="email"/>
                     </div>
                     <div className="Field">
                         <label>
                             Password <sup>*</sup>
                         </label>
-                        <input placeholder="password"/>
+                        <input  value={password.value} 
+                        onChange= {e => 
+                        {setPassword({...password, value:e.target.value});}}
+                        onBlur={ setPassword({...password, isTouched:True})}
+                        placeholder="password"/>
                     </div>
                     <div className="Field">
                         <label>
                             Role <sup>*</sup>
                         </label>
-                        <select > 
+                        <select value={role} 
+                        onChange={ e => setRole(e.target.value)}> 
                             <option value="role"> Role </option>
                             <option value="individuals">Individuals</option>
                             <option value="business">Business</option>
