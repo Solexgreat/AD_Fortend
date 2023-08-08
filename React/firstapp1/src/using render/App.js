ddc632e740594a29a6ae13c97fb1c4db88f1b539
Solexgreat <solomonGodkid@gmail.com>
2023-08-08 17:39:29 +0100

    Using render

--- React/firstapp1/src/using render/App.js
+++ React/firstapp1/src/using render/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { render } from '@testing-library/react';
 
 const MousePosition = ({ render }) => {
   const [mousePosition, setMousePosition] = useState({
@@ -20,32 +21,27 @@ const MousePosition = ({ render }) => {
   }, []);
 
   // What should be returned here?
-  return null;
+  render(mousePosition);
 };
 
 // This component should not receive any props
 const PanelMouseLogger = () => {
-  // The below if statement can be removed after the render props pattern is implemented
-  if (!mousePosition) {
-    return null;
-  }
   return (
     <div className="BasicTracker">
       <p>Mouse position:</p>
-      <div className="Row">
+      <MousePosition render={({mousePosition}) => (
+       <div className="Row">
         <span>x: {mousePosition.x}</span>
         <span>y: {mousePosition.y}</span>
-      </div>
+       </div> 
+      )}/>
+      
     </div>
   );
 };
 
 // This component should not receive any props
 const PointMouseLogger = ({mousePosition}) => {
-  // The below if statement can be removed after the render props pattern is implemented
-  if (!mousePosition) {
-    return null;
-  }
   return (
     <p>
       ({mousePosition.x}, {mousePosition.y})
