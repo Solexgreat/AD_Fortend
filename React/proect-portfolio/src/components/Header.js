2f70e4e5c95671533e98c1e7415e0b6ee5286155
Solexgreat <solomonGodkid@gmail.com>
2023-08-24 10:37:05 +0100

    Project Portfolio

--- React/proect-portfolio/src/components/Header.js
+++ React/proect-portfolio/src/components/Header.js
@@ -49,52 +49,55 @@ function Header() {
 
   return(
     <ChakraProvider >
-      <Box 
-      pos="fixed"
-      top={0}
-      left={0}
-      right={0}
-      translateY={0}
-      transitionProperty="transform"
-      transitionDuration=".3s"
-      transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b" 
-      >
-        <Box color='dark-blue' maxW="1280px" m="0 auto">
-          <HStack
-          spacing="16px"
-          px={16}
-          py={4}
-          justifyContent="space-between"
-          alignItems="center"
-          >
-            <nav>
-              <HStack spacing={10}>
-                <a href={socials[0].url }>
-                  <FontAwesomeIcon icon={socials[0].icon} size="2x" />
-                </a>
-                <a href={socials[1].url}>
-                  <FontAwesomeIcon icon={socials[1].icon}  size="2x"/>
-                </a>
-                <a href={socials[2].url}>
-                  <FontAwesomeIcon icon={socials[2].icon} size="2x"/>
-                </a>
-                <a href={socials[3].url}>
-                  <FontAwesomeIcon icon={socials[3].icon} size="2x"/>
-                </a> 
-                <a href={socials[4].url}>
-                  <FontAwesomeIcon icon={socials[4].icon} size="2x"/>
-                </a>  
-              </HStack>       
-            </nav>
-            <nav>
-              <HStack spacing={10}>
-                <a href='/ContactMeSection' onClick={handleclick('contactme')}>Contact Me</a>
-                <a href="/ProjectsSection" onClick={handleclick('projects')}>Projects</a>
+      <Box
+      bg="darkblue">
+        <Box 
+        pos="fixed"
+        top={0}
+        left={0}
+        right={0}
+        translateY={0}
+        transitionProperty="transform"
+        transitionDuration=".3s"
+        transitionTimingFunction="ease-in-out"
+        backgroundColor="#18181b" 
+        >
+          <Box color='white' maxW="1280px" m="0 auto">
+            <HStack
+            spacing="16px"
+            px={16}
+            py={4}
+            justifyContent="space-between"
+            alignItems="center"
+            >
+              <nav>
+                <HStack spacing={5}>
+                  <a href={socials[0].url }>
+                    <FontAwesomeIcon icon={socials[0].icon} size="2x" />
+                  </a>
+                  <a href={socials[1].url}>
+                    <FontAwesomeIcon icon={socials[1].icon}  size="2x"/>
+                  </a>
+                  <a href={socials[2].url}>
+                    <FontAwesomeIcon icon={socials[2].icon} size="2x"/>
+                  </a>
+                  <a href={socials[3].url}>
+                    <FontAwesomeIcon icon={socials[3].icon} size="2x"/>
+                  </a> 
+                  <a href={socials[4].url}>
+                    <FontAwesomeIcon icon={socials[4].icon} size="2x"/>
+                  </a>  
+                </HStack>       
+              </nav>
+              <nav>
+                <HStack spacing={10}>
+                  <a href='/ContactMeSection' onClick={handleclick('contactme')}>Contact Me</a>
+                  <a href="/ProjectsSection" onClick={handleclick('projects')}>Projects</a>
 
-              </HStack>
-            </nav>
-          </HStack>
+                </HStack>
+              </nav>
+            </HStack>
+          </Box>
         </Box>
       </Box>
     </ChakraProvider>
