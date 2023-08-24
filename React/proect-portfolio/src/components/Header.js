ff6fe9eda081c1b33e5846ef5dbe34300f1a0183
Solexgreat <solomonGodkid@gmail.com>
2023-08-24 10:05:50 +0100

    Project Portfolio

--- React/proect-portfolio/src/components/Header.js
+++ React/proect-portfolio/src/components/Header.js
@@ -10,73 +10,92 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 //import theme from "./theme";
 
-function Header() {
 
-  const socials =[
-    {
-      icon: faGithub,
-      url: "https://github.com",
-    },
-    {
-      icon: faEnvelope,
-      url: "mailto: hello@example.com",
-    },
-    {
-      icon: faLinkedin,
-      url: "https://www.linkedin.com",
-    },
-    {
-      icon: faMedium,
-      url: "https://medium.com",
-    },
-    {
-      icon: faStackOverflow,
-      url: "https://stackoverflow.com",
-    },
-  ]
-    return(
-      <ChakraProvider >
-        <Box 
-        pos="fixed"
-        top={0}
-        left={0}
-        right={0}
-        translateY={0}
-        transitionProperty="transform"
-        transitionDuration=".3s"
-        transitionTimingFunction="ease-in-out"
-        backgroundColor="#18181b" 
-        >
-          <Box color='white' maxW="1280px" m="0 auto">
-            <HStack
-            spacing="5px"
-            px={16}
-            py={4}
-            justifyContent="space-between"
-            alignItems="center"
-            >
-              <nav>         
-                <a href={socials[0].url }>
-                  <FontAwesomeIcon icon={socials[0].icon} size="2x" />
-                </a>
-                <a href={socials[1].url}>
-                  <FontAwesomeIcon icon={socials[1].icon}  size="2x"/>
-                </a>
-                <a href={socials[2].url}>
-                  <FontAwesomeIcon icon={socials[2].icon} size="2x"/>
-                </a>
-                <a href={socials[3].url}>
-                  <FontAwesomeIcon icon={socials[3].icon} size="2x"/>
-                </a> 
-                <a href={socials[4].url}>
-                  <FontAwesomeIcon icon={socials[4].icon} size="2x"/>
-                </a>
-              </nav>
-            </HStack>
-          </Box>
+const socials =[
+  {
+    icon: faGithub,
+    url: "https://github.com",
+  },
+  {
+    icon: faEnvelope,
+    url: "mailto: hello@example.com",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com",
+  },
+  {
+    icon: faMedium,
+    url: "https://medium.com",
+  },
+  {
+    icon: faStackOverflow,
+    url: "https://stackoverflow.com",
+  },
+]
+
+function Header() {
+  const handleclick = (achor) =>{
+    const id = `${achor}-section`;
+    const element = document.getElementById(id)
+    if (element){
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      })
+    }
+  }
+
+  return(
+    <ChakraProvider >
+      <Box 
+      pos="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b" 
+      >
+        <Box color='white' maxW="1280px" m="0 auto">
+          <HStack
+          spacing="16px"
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+          >
+            <nav>         
+              <a href={socials[0].url }>
+                <FontAwesomeIcon icon={socials[0].icon} size="2x" />
+              </a>
+              <a href={socials[1].url}>
+                <FontAwesomeIcon icon={socials[1].icon}  size="2x"/>
+              </a>
+              <a href={socials[2].url}>
+                <FontAwesomeIcon icon={socials[2].icon} size="2x"/>
+              </a>
+              <a href={socials[3].url}>
+                <FontAwesomeIcon icon={socials[3].icon} size="2x"/>
+              </a> 
+              <a href={socials[4].url}>
+                <FontAwesomeIcon icon={socials[4].icon} size="2x"/>
+              </a>
+            </nav>
+            <nav>
+              <HStack spacing={10}>
+                <a href='/ContactMeSection'>Contact Me</a>
+                <a href="/Projects">Projects</a>
+
+              </HStack>
+            </nav>
+          </HStack>
         </Box>
-      </ChakraProvider>
-    );
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default Header;
