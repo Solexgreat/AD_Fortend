e23f0c8704bcf4ad9cc37d1fc417a0dcdc6cf5da
Solexgreat <solomonGodkid@gmail.com>
2023-08-31 04:54:48 +0100

    Project Portfolio

--- React/proect-portfolio/src/context/alertContext.js
+++ React/proect-portfolio/src/context/alertContext.js
@@ -1,1 +1,20 @@
 import { useState, createContext, useContext } from "react";
+
+const AlertContext = createContext(undefined);
+
+const AlertProvide = () =>{
+    const [state, setState] = useState({
+        isOpen: false,
+        type: "success",
+        message:''
+    })
+
+    return(
+        <AlertContext.Provider
+        value={{
+            ...state,
+            onOpen: (type, message) => {setState({isOpen: true, type, message})}
+            onClose
+        }} ></AlertContext.Provider>
+    )
+}
