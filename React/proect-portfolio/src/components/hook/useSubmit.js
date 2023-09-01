9a224b230c16e48c3853467fcfcd83ceb9658ac5
Solexgreat <solomonGodkid@gmail.com>
2023-09-01 03:57:26 +0100

    Project Portfolio

---
+++ React/proect-portfolio/src/components/hook/useSubmit.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+
+const useSubmit = () =>{
+    const[isLoading, setLoading] = useState(false);
+    const [Response, setResponse] = useState(null);
+
+    const submit = async (url, data) =>{
+        const random = Math.random();
+        setLoading(true)
+        try{
+            await wait(2000);
+            if (random < 0.5) {
+                throw new error('')
+            }
+
+        }
+    }
+}
